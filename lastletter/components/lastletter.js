6e7929b5a23d98935ec8e6c950115c3d9afbe5c5
carloshosiqueira <carlos.siqueira.des@gmail.com>
2024-07-26 10:19:21 -0300

    Add tela de login

---
+++ lastletter/components/lastletter.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const LastLetter = () => {
+    return (
+        <Text>A</Text>
+    )
+}
+
+export default LastLetter;
